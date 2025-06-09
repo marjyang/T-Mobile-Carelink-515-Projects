3aed31c67da4aa98b2a4f1dc3b8f5607ad771ac9
Marjorie Yang <78562573+marjyang@users.noreply.github.com>
2025-06-09 11:47:23 -0700

    Delete Carelink_Pillbox directory

--- Carelink_Pillbox/portal/src/components/Patient/Spinner.jsx
+++
@@ -1,7 +0,0 @@
-export default function Spinner({ size = 40 }) {
-  return (
-    <div className="spinner-container">
-      <div className="spinner" style={{ width: size, height: size }} />
-    </div>
-  );
-}
